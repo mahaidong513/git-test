e4dee526a2cc900a89ddfc579b6da092b37d1353
mhd <776741417@qq.com>
2024-03-19 16:55:54 +0800

    third commit

--- src/main/java/com/haidong/git/GitTest.java
+++ src/main/java/com/haidong/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git2");
+        System.out.println("git3");
 
     }
 }
